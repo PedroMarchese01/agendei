f18c8eef579ab41dd78532e25ca5da709976ff8e
Pedro Marchese <rm563339@fiap.com.br>
2025-10-20 21:07:33 -0300

    FEAT: Responsividade ajustada

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,10 @@
 const page = () => {
   return (
-    <div className="grid grid-cols-1 grid-rows-1">
+    <div className="grid grid-cols-1 h-full grid-rows-1 md:grid-cols-3">
         <section>
             <p>Login</p>
         </section>
-        <section className="hidden md:block">
+        <section className="hidden md:block md:col-span-2">
             <p>imagem</p>
         </section>
     </div>

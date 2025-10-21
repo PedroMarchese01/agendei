be2e45bcf31584170b2af2b8caaa14953f0f1344
Pedro Marchese <rm563339@fiap.com.br>
2025-10-20 21:23:08 -0300

    FEAT: Estrutura Login Realizada

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,17 @@
 const page = () => {
   return (
-    <div className="grid grid-cols-1 h-full grid-rows-1 md:grid-cols-3">
-        <section>
-            <p>Login</p>
+    <div className="grid grid-cols-1 h-screen grid-rows-1 md:grid-cols-3">
+        <section className="flex flex-col justify-start bg-[#f9f9f9] items-center w-full">
+           <div className="flex flex-col items-center">
+                <h1>Agendei!</h1>
+                <h2>Login</h2>
+                <p>barra progresso</p>
+           </div>
+           <div>
+                {/* componente  */}
+           </div>
         </section>
-        <section className="hidden md:block md:col-span-2">
+        <section className="hidden md:block md:col-span-2 bg-[#1c1c1c] w-full">
             <p>imagem</p>
         </section>
     </div>

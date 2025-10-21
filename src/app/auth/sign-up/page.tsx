bc3d6d5fe2bef2a439e3bd3a102a2ebe74eb2c66
Pedro Marchese <rm563339@fiap.com.br>
2025-10-21 13:19:25 -0300

    CHORE: remocão de conteudo desnecessario

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,16 +1,4 @@
 const page = () => {
-    const user = {
-        id:"",
-        name:"",
-        cpf:"",
-        email:"",
-        cellphone:"",
-        password:"",
-        gender:"",
-        birthday:"",
-        role:"",
-        storesId:[],
-    }
   return (
     <div>
 

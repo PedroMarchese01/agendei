f5c09f4763083fe08f3cb4b9c744a42cf3fd38e1
Pedro Marchese <rm563339@fiap.com.br>
2025-10-22 13:34:16 -0300

    FEAT: Backend login totalmente realizado

--- src/app/auth/login/components/field-login.tsx
+++ src/app/auth/login/components/field-login.tsx
@@ -1,83 +1,109 @@
-  "use client"
-  import { Button } from "@/components/ui/button"
-  import { Input } from "@/components/ui/input"
-  import { Checkbox } from "../../../../components/ui/checkbox"
-  import { Label } from "../../../../components/ui/label"
-  import { useEffect, useState } from "react"
+"use client"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Checkbox } from "../../../../components/ui/checkbox"
+import { Label } from "../../../../components/ui/label"
+import { useEffect, useState } from "react"
+import {User} from "../../../../models/User"
 
-  const Field = () => {
-    
-    const emailRegex = /^[^\s@]+@[^\s@]+\.(com)(\.br)?$/i
-    const api = "/api/users"
-    const [UsersData , setUserData] = useState([])
-    const [formLogin, setFormLogin] = useState({
-      email: "",
-      validEmail: false,
-      password: "",
-    })
 
-    const [showPassword, setShowPassword] = useState(false)
+interface LoginForm {
+  email: string;
+  validEmail: boolean;
+  password: string;
+}
 
-    useEffect(() => {
-      fetch(`${api}`)
+const Field = () => {
+  
+  const emailRegex = /^[^\s@]+@[^\s@]+\.(com)(\.br)?$/i
+  const api = "/api/users"
+  const [UsersData , setUserData] = useState<User[]>([])
+  const [formLogin, setFormLogin] = useState<LoginForm>({
+    email: "",
+    validEmail: false,
+    password: "",
+  })
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  useEffect(() => {
+    fetch(`${api}`)
       .then((res) => res.json())
-        .then((json) => setUserData(json))
-        .catch((err) => console.error(err))
-    }, []) // [] = roda apenas uma vez
+      .then((json) => setUserData(json))
+      .catch((err) => console.error(err))
+  }, []) // [] = roda apenas uma vez
 
-    return (
-      <>
-        <article className="flex flex-col items-center w-full px-4">
-          <div className="flex flex-col w-full items-center mb-2">
-            {/* Email */}
-            <Input
-              type="email"
-              placeholder="Digite seu email"
-              value={formLogin.email}
-              onChange={(e) =>
-                setFormLogin((prev) => ({
-                  ...prev,
-                  email: e.target.value,
-                  validEmail: emailRegex.test(e.target.value),
-                }))
-              }
-            />
+  return (
+    <>
+      <article className="flex flex-col items-center w-full px-4">
+        <div className="flex flex-col w-full items-center mb-2">
+          {/* Email */}
+          <Input
+            type="email"
+            placeholder="Digite seu email"
+            value={formLogin.email}
+            onChange={(e) =>
+              setFormLogin((prev) => ({
+                ...prev,
+                email: e.target.value,
+                validEmail: emailRegex.test(e.target.value),
+              }))
+            }
+          />
 
-            {/* Senha */}
-            <Input
-              type={showPassword ? "text" : "password"}
-              placeholder="Digite sua senha"
-              value={formLogin.password}
-              onChange={(e) =>
-                setFormLogin((prev) => ({
-                  ...prev,
-                  password: e.target.value,
-                }))
-              }
-            />
-          </div>
+          {/* Senha */}
+          <Input
+            type={showPassword ? "text" : "password"}
+            placeholder="Digite sua senha"
+            value={formLogin.password}
+            onChange={(e) =>
+              setFormLogin((prev) => ({
+                ...prev,
+                password: e.target.value,
+              }))
+            }
+          />
+        </div>
 
-          {/* Mostrar senha */}
-          <div className="flex gap-2 items-center mt-2 ">
-            <Checkbox
-              checked={showPassword}
-              onCheckedChange={() => setShowPassword(!showPassword)}
-            />
-            <Label>Mostrar Senha</Label>
-          </div>
-        </article>
+        {/* Mostrar senha */}
+        <div className="flex gap-2 items-center mt-2 ">
+          <Checkbox
+            checked={showPassword}
+            onCheckedChange={() => setShowPassword(!showPassword)}
+          />
+          <Label>Mostrar Senha</Label>
+        </div>
+      </article>
 
-        <footer className="flex flex-col mt-2 w-full p-4 gap-4">
-          <Button onClick={() => {!formLogin.validEmail?alert("email invalido") : 
-            // colocar lógica para busca
-            alert("enviado")
-          }}>Enviar</Button>
-          <p className="text-center flex justify-center">
-            Esqueceu a senha? Clique aqui!
-          </p>
-        </footer>
-      </>
-    )
-  }
+      <footer className="flex flex-col mt-2 w-full p-4 gap-4">
+        <Button onClick={() => {
+          if (!formLogin.validEmail) {
+            alert("Email inválido");
+            return;
+          }
 
-  export default Field
+          const usuario = UsersData.find((item) => item.email === formLogin.email);
+
+          if (!usuario) {
+            alert("Usuário não encontrado!");
+            return;
+          }
+
+          if (usuario.password !== formLogin.password) {
+            alert("Senha incorreta!");
+            return;
+          }
+
+          alert("Login válido! Usuário autenticado.");
+        }}>
+          Enviar
+        </Button>
+        <p className="text-center flex justify-center">
+          Esqueceu a senha? Clique aqui!
+        </p>
+      </footer>
+    </>
+  )
+}
+
+export default Field
